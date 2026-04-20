77491d19f779dbe16197783832de8ce2a156ef8b
jennifernabiila <jennifernabiila26@gmail.com>
2026-04-20 09:49:24 +0700

    pertemuan 7

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('halo', function () {
+	return "<h1>Halo, Selamat datang</h1> di tutorial laravel <i>www.malasngoding.com</i>";
+});
+
+Route::get('blog', function () {
+    return view('blog');
+});
+
+Route::get('pert5', function () {
+    return view('pertemuan5');
+});
