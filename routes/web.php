b9ce744337e11a0a9d3bcb31655a328287a97771
jennifernabiila <jennifernabiila26@gmail.com>
2026-04-20 10:18:11 +0700

    pertemuan 7

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DosenController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,6 +15,23 @@ Route::get('blog', function () {
     return view('blog');
 });
 
-Route::get('pert5', function () {
+//Tugas
+Route::get('pert-1', function () {
     return view('pertemuan5');
 });
+Route::get('pert-2', function () {
+    return view('pertemuan5');
+});
+Route::get('pert-3', function () {
+    return view('pertemuan5');
+});
+Route::get('pert-4', function () {
+    return view('pertemuan5');
+});
+Route::get('pert-5', function () {
+    return view('pertemuan5');
+});
+
+
+Route::get('dosen', [DosenController::class, 'index']);
+Route::get('biodata', [DosenController::class, 'biodata']);
